67a277ba4f375ecd4bf240482b02ad7d1ed2161e
Pottttle <1794286022@qq.com>
2022-12-24 11:38:42 +0800

    update

---
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/AutoJump.kt
@@ -0,0 +1,26 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
+
+class AutoJump : SpeedMode("AutoJump") {
+    override fun onDisable() {
+        mc.timer.timerSpeed = 1f
+        mc.thePlayer!!.speedInAir = 0.02f
+    }
+    override fun onTick() {}
+    override fun onMotion() {}
+    override fun onUpdate() {
+        val thePlayer = mc.thePlayer ?: return
+
+
+        if (thePlayer.onGround) {
+            if(MovementUtils.isMoving())
+                thePlayer.jump()
+        }
+    }
+    override fun onMove(event: MoveEvent) {}
+    override fun onEnable() {}
+}
